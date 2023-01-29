1e01314ec14a27383c6a05a86f879cfffdc3b11f
luizgmelo <88911920+luizgmelo@users.noreply.github.com>
2023-01-29 14:23:28 -0300

    adicionado mais imagens e corrigido bug de mesmo pares

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const cards = document.querySelector("div.cards")
-const arrayCardsId = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12] 
+const arrayCardsId = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
+                      13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24] 
 const match = []
-const differenceId = 6;
+const differenceId = 12;
 let score = 0;
 
 // randomize cards
@@ -17,11 +18,20 @@ arrayCardsId.forEach((cardId) => {
 function resetCards() {
     let firstCard = document.getElementById(match[0].id);
     let secondCard = document.getElementById(match[1].id);
-    firstCard.src = "images/card.png";
-    secondCard.src =  "images/card.png";
+    if (firstCard.alt == "face-card" && secondCard.alt != "face-card") {
+        alert("A primeira carta já foi virada, por favor escolha outra")
+        secondCard.src = "images/card.png";
+    } else if (secondCard.alt == "face-card" && firstCard.alt != "face-card"){
+        alert("A segunda carta já foi virada, por favor escolha outra")
+        firstCard.src = "images/card.png";
+    } else if (firstCard.alt != "face-card" && secondCard.alt != "face-card") {
+        firstCard.src = "images/card.png";
+        secondCard.src = "images/card.png";
+    }
+
 
     // clear array
-    match.splice(0, 12);
+    match.splice(0, 24);
 }
 
 function flipCard(id) {
@@ -36,12 +46,16 @@ function flipCard(id) {
     }
     
     if (match.length == 2) {
-        if (Number(match[0].id) + differenceId == Number(match[1].id) || Number(match[0].id) == Number(match[1].id) + differenceId) {
+        if ((Number(match[0].id) + differenceId == Number(match[1].id) || Number(match[0].id) == Number(match[1].id) + differenceId) && card.alt != "face-card") {
+            let firstCard = document.getElementById(match[0].id);
+            let secondCard = document.getElementById(match[1].id);
+            firstCard.alt = "face-card"
+            secondCard.alt = "face-card"
             score++
             alert("Incrível")
             match.pop()
             match.pop()
-            if (score == 6) {
+            if (score == 12) {
                 alert("Parabéns você concluiu o jogo")
                 let title = document.getElementById("title")
                 title.style.color = "#ff0000"
