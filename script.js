8426f47d666946ee94b245276a1aa695b293540f
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2023-12-02 09:17:29 -0300

    refactor

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-const cards = document.querySelector("div.cards")
+const divCards = document.querySelector(".cards")
 const arrayCards = ["bakugo", "kirishima", "lida", "mic", 
-                    "midoriya", "might", "neito", "ojiro", 
-                    "shoji", "todoroki", "tokoyami", "uraraka"];
+  "midoriya", "might", "neito", "ojiro", 
+  "shoji", "todoroki", "tokoyami", "uraraka"];
 arrayCards.push(...arrayCards);
+
 const match = [];
 let score = 0;
 let cardId = 0;
@@ -13,55 +14,60 @@ title.innerText += ` score: ${score}`
 // randomize cards
 arrayCards.sort(() => 0.5 - Math.random())
 
-console.log(arrayCards);
-
-
 arrayCards.forEach((character) => {
-    cardId++
-    cards.innerHTML += `<img id="${cardId}" src="images/card.png" 
-                        alt="card-blank" onclick="flipCard('${cardId}')" data-character="${character}">`
-}) 
+  cardId++
+  const img = document.createElement("img");
+  img.setAttribute("id", cardId);
+  img.className = "card";
+  img.setAttribute("src", "images/card.png");
+  img.addEventListener('click', flipCard);
+  img.setAttribute("alt", "card-black");
+  img.setAttribute("data-character", character);
+  divCards.appendChild(img);
+})
 
 function resetCards() {
-    let firstCard = document.getElementById(match[0].id)
-    let secondCard = document.getElementById(match[1].id)
-    firstCard.src = "images/card.png";
-    secondCard.src = "images/card.png";
-    match.pop()
-    match.pop()
+  let firstCard = document.getElementById(match[0].id)
+  let secondCard = document.getElementById(match[1].id)
+  firstCard.src = "images/card.png";
+  secondCard.src = "images/card.png";
+  match.pop()
+  match.pop()
 }
 
-function flipCard(cardId) {
-    let card = document.getElementById(cardId);
-    if (!match.includes(card) && match.length < 2 && card.alt != 'front-face') {
-        match.push(card);
-        card.setAttribute('src', `images/${card.dataset.character}.png`)
-    } 
-    if (match.length > 2){
-        return alert("You only can to turn two cards at same time");
-    }
-    if (match.length == 2) {
-        
-        let firstCard = match[0];
-        let secondCard = match[1];
+function flipCard() {
+  let cardId = this.getAttribute('id')
+  let card = document.getElementById(cardId)
+  if (!match.includes(card) && match.length < 2 && card.alt != 'front-face') {
+    match.push(card);
+    card.setAttribute('src', `images/${card.dataset.character}.png`)
+  } 
+  if (match.length > 2){
+    return alert("You only can to turn two cards at same time");
+  }
+  if (match.length == 2) {
 
-        if (firstCard.dataset.character == secondCard.dataset.character) {
-            firstCard.alt = 'front-face'
-            secondCard.alt = 'front-face'
-            score++
-            title.innerText = `Memory Game score: ${score}`
-            alert("Awesome")
-            match.pop()
-            match.pop()
-            if (score == 12) {
-                alert("Congradulations you complete the game.")
-                let title = document.getElementById("title")
-                title.style.color = "#ff0000"
-                title.innerText = "restart the page to play again"
-            }
-        } else {
-            setTimeout(resetCards, 2000)
-        }
-    }
+    let firstCard = match[0];
+    let secondCard = match[1];
 
+    if (firstCard.dataset.character == secondCard.dataset.character) {
+      firstCard.alt = 'front-face'
+      secondCard.alt = 'front-face'
+      score++
+      title.innerText = `Memory Game score: ${score}`
+      alert("Awesome")
+      firstCard.removeEventListener("click", flipCard)
+      secondCard.removeEventListener("click", flipCard)
+      match.pop()
+      match.pop()
+      if (score == 12) {
+        alert("Congradulations you complete the game.")
+        let title = document.getElementById("title")
+        title.style.color = "#ff0000"
+        title.innerText = "restart the page to play again"
+      }
+    } else {
+      setTimeout(resetCards, 2000)
+    }
+  }
 }
