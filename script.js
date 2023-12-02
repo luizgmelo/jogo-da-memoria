18052b1106d029c3a097b32ff507231ab7280c27
Luiz Guilherme <88911920+luizgmelo@users.noreply.github.com>
2023-12-02 11:24:01 -0300

    refactor

--- script.js
+++ script.js
@@ -9,22 +9,24 @@ let score = 0;
 let cardId = 0;
 
 let title = document.querySelector("#title");
-title.innerText += ` score: ${score}` 
+title.innerText += ' ' + score
 
 // randomize cards
 arrayCards.sort(() => 0.5 - Math.random())
 
-arrayCards.forEach((character) => {
-  cardId++
-  const img = document.createElement("img");
-  img.setAttribute("id", cardId);
-  img.className = "card";
-  img.setAttribute("src", "images/card.png");
-  img.addEventListener('click', flipCard);
-  img.setAttribute("alt", "card-black");
-  img.setAttribute("data-character", character);
-  divCards.appendChild(img);
-})
+function createBoard() {
+  arrayCards.forEach((character) => {
+    cardId++
+    const img = document.createElement("img");
+    img.setAttribute("id", cardId);
+    img.className = "card";
+    img.setAttribute("src", "images/card.png");
+    img.addEventListener('click', flipCard);
+    img.setAttribute("alt", "card-black");
+    img.setAttribute("data-character", character);
+    divCards.appendChild(img);
+  })
+}
 
 function resetCards() {
   let firstCard = document.getElementById(match[0].id)
@@ -39,35 +41,35 @@ function flipCard() {
   let cardId = this.getAttribute('id')
   let card = document.getElementById(cardId)
   if (!match.includes(card) && match.length < 2 && card.alt != 'front-face') {
-    match.push(card);
     card.setAttribute('src', `images/${card.dataset.character}.png`)
+    match.push(card);
   } 
-  if (match.length > 2){
-    return alert("You only can to turn two cards at same time");
-  }
-  if (match.length == 2) {
-
-    let firstCard = match[0];
-    let secondCard = match[1];
-
-    if (firstCard.dataset.character == secondCard.dataset.character) {
-      firstCard.alt = 'front-face'
-      secondCard.alt = 'front-face'
-      score++
-      title.innerText = `Memory Game score: ${score}`
-      alert("Awesome")
-      firstCard.removeEventListener("click", flipCard)
-      secondCard.removeEventListener("click", flipCard)
-      match.pop()
-      match.pop()
-      if (score == 12) {
-        alert("Congradulations you complete the game.")
-        let title = document.getElementById("title")
-        title.style.color = "#ff0000"
-        title.innerText = "restart the page to play again"
-      }
-    } else {
-      setTimeout(resetCards, 2000)
-    }
+  if (match.length === 2) {
+    setTimeout(checkMatch, 500);
   }
 }
+
+function checkMatch() {
+  let firstCard = match[0];
+  let secondCard = match[1];
+  if (firstCard.dataset.character == secondCard.dataset.character) {
+    firstCard.alt = 'front-face'
+    secondCard.alt = 'front-face'
+    score++
+    title.innerText = `Memory Game score: ${score}`
+    alert("Awesome")
+    firstCard.removeEventListener("click", flipCard)
+    secondCard.removeEventListener("click", flipCard)
+    match.pop()
+    match.pop()
+    if (score === 12) {
+      alert("Congradulations you complete the game.")
+      let title = document.getElementById("title")
+      title.style.color = "#ff0000"
+      title.innerText = "restart the page to play again"
+    }
+  } else {
+    setTimeout(resetCards, 2000)
+  }
+}
+createBoard()
